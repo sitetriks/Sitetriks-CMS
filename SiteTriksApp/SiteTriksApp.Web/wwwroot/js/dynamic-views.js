fe648364f7862f1f7204c0ff37000d7796efa71c
KKaloyan <mail@kaloyanit.com>
2018-06-06 18:57:03 +0300

    Dynamic views changes

---
+++ SiteTriksApp/SiteTriksApp.Web/wwwroot/js/dynamic-views.js
@@ -0,0 +1,32 @@
+﻿var DynamicViews = function () {
+    return {
+        setupWidgetNameDropdown: function (selector) {
+            $(selector).change(function () {
+                let $location = $("#location");
+                let $info = $("#custom-location-info");
+
+                if ($(this).val() == "none") {
+                    $location.val("");
+                    $info.text("");
+                    $info.attr("hidden", "hidden");
+                    $location.removeAttr("hidden");
+                }
+                else if ($(this).val().indexOf("dynamic/") != -1) {
+                    let dynamicClassName = $(this).val().replace("dynamic/", "");
+                    $location.removeAttr("hidden");
+                    $info.removeAttr("hidden");
+
+                    $location.val("Views/Shared/Components/DynamicWidget/" + dynamicClassName + "/{ViewType}/{ViewName}.cshtml");
+                    $("#custom-location-info").text("Change {ViewType} and {ViewName} placeholders!")
+                }
+                else {
+                    $location.attr("hidden", "hidden");
+                    $info.attr("hidden", "hidden");
+
+                    $location.val("");
+                    $info.val("");
+                }
+            });
+        }
+    };
+}();
